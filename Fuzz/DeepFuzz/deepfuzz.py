f0f16ce4534fedf1c40d1b8ed62925f564a32768
EJueon <jueoneom@yonsei.ac.kr>
2023-06-18 15:00:25 +0900

    update deepfuzz training module

--- Fuzz/DeepFuzz/deepfuzz.py
+++ Fuzz/DeepFuzz/deepfuzz.py
@@ -1,9 +1,11 @@
 import sys
 import os
+import torch
+import wandb
 import argparse
 from tqdm import tqdm
 from torch.utils.data import DataLoader
-from torch.optim import AdamW
+from torch.optim import AdamW, lr_scheduler
 import pandas as pd
 
 from seq2seq import Seq2Seq, DeepFuzzDataset
@@ -34,31 +36,51 @@ def preprocess(dataset_path : str, save_dir: str, max_seq_len : int = 50) -> pd.
     new_dataset = pd.DataFrame(sequences, columns=["data"])
     new_dataset.to_json(os.path.join(save_dir, os.path.basename(dataset_path)), orient="records", force_ascii=False)
 
-def train(train_dataset_path : str, dev_dataset_path: str, save_dir: str, max_seq_len : int = 50, batch_size=64, lr=0.001, epochs=100):
+def train(train_dataset_path : str, dev_dataset_path: str, save_dir: str, max_seq_len : int = 50, vocab_size=98, batch_size=4096, lr=2e-4, epochs=50):
     check_dir(save_dir)
     train_dataset = pd.read_json(train_dataset_path)
     dev_dataset = pd.read_json(dev_dataset_path)
+    wandb.init(project="deepfuzz")
     
-    train_dataloader = DataLoader(DeepFuzzDataset(dataset=train_dataset), batch_size=batch_size, shuffle=True)
-    dev_dataloader = DataLoader(DeepFuzzDataset(dataset=dev_dataset), batch_size=batch_size, shuffle=False)
     
-    model = Seq2Seq(98, 512, 512, 2, 0.2)
-    optimizer = AdamW(lr=lr, weight_decay=0.01, eps=1e-08)
-    for epoch in tqdm(range(epochs)):
+    train_dataloader = DataLoader(DeepFuzzDataset(dataset=train_dataset, max_seq_len=max_seq_len, vocab_size=vocab_size), batch_size=batch_size, shuffle=True)
+    dev_dataloader = DataLoader(DeepFuzzDataset(dataset=dev_dataset, max_seq_len=max_seq_len, vocab_size=vocab_size), batch_size=batch_size, shuffle=False)
+
+    # 96: start, 97: end
+    model = Seq2Seq(vocab_size=vocab_size, hidden_dim=512, n_layers=2, dropout=0.0)
+    model.to('cuda')
+    wandb.watch(model, model.loss_func, log="all", log_freq=10)
+    optimizer = AdamW(params=model.parameters(), lr=lr, weight_decay=0.01, eps=1e-08,)
+    scheduler = lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=1, T_mult=2, eta_min=0.001)
+    
+    for epoch in range(epochs):
         
-        optimizer.zero_grad()
+        
         model.train()
-        for batch in train_dataloader:
+        
+        iter_bar = tqdm(train_dataloader)
+        for batch in iter_bar:
+            optimizer.zero_grad()
+            batch = torch.stack([b.to("cuda") for b in batch])
             output, loss = model(batch)
+            
             loss.backward()
             optimizer.step()
-            print(loss)
+            scheduler.step()
+            wandb.log({"epoch": epoch, "train loss": loss, "lr": optimizer.param_groups[0]["lr"] })
+            iter_bar.set_description('Train Iter (lr=%5.5f, loss=%5.3f)'%(optimizer.param_groups[0]["lr"], loss.item()))
+            torch.cuda.empty_cache()
             
+        torch.save(model.state_dict(), os.path.join(save_dir, f"epoch_{epoch}"))
         model.eval()
-        for batch in dev_dataloader:
-            output, loss = model(batch)
-            print(loss)
-            
+        with torch.no_grad():
+            iter_bar = tqdm(dev_dataloader)
+            for batch in dev_dataloader:
+                batch = torch.stack([b.to("cuda") for b in batch])
+                output, loss = model(batch)
+                wandb.log({"epoch": epoch, "eval loss": loss})
+                iter_bar.set_description('Dev Iter (lr=%5.5f, loss=%5.3f)'%(optimizer.param_groups[0]["lr"], loss.item()))
+                torch.cuda.empty_cache()
         
     
     
@@ -76,8 +98,8 @@ if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument("--mode", required=True, type=str, choices=["preprocess", "train", "fuzz"])
     argparser.add_argument("--save_dir", required=False, type=str, default="./save")
-    argparser.add_argument("--train_dataset_path", required=False, type=str, default="")
-    argparser.add_argument("--dev_dataset_path", required=False, type=str, default="")
+    argparser.add_argument("--train_dataset_path", required=False, type=str, default="./save/train_dataset.json")
+    argparser.add_argument("--dev_dataset_path", required=False, type=str, default="./save/dev_dataset.json")
     argparser.add_argument("--max_seq_len", required=False, type=int, default=50)
     args = argparser.parse_args(sys.argv[1:])
     
--- Fuzz/DeepFuzz/seq2seq.py
+++ Fuzz/DeepFuzz/seq2seq.py
@@ -2,72 +2,91 @@ import torch
 import random
 from torch import nn
 from torch.utils.data import Dataset
+import torch.nn.functional as F
 
 class DeepFuzzDataset(Dataset):
-    def __init__(self, dataset):
+    def __init__(self, dataset, max_seq_len, vocab_size):
         super().__init__()
-        self.dataset = dataset
+        self.dataset = dataset["data"]
+        self.vocab_size = vocab_size
+        self.max_seq_len = max_seq_len + 1
     
     
     def char_to_ord(self, sentence):
-        data = []
+        data = [96]
         for s in sentence:
-            data.append(ord(s))
+            if ord(s) < self.vocab_size:
+                data.append(ord(s)-32)
+        data.append(97)
         return data
+    
+    def pad_id(self, x):
+        if len(x) < self.max_seq_len:
+            pad = [0] * (self.max_seq_len - len(x))
+            x.extend(pad)
+        return x
     
     def __len__(self):
         return len(self.dataset)
     
-    def get_item(self, idx):
-        return self.char_to_ord(self.dataset[idx])
+    def __getitem__(self, idx):
+        data = self.char_to_ord(self.dataset[idx])
+    
+        x = F.one_hot(torch.tensor(self.pad_id(data[:-1]), dtype=torch.long), num_classes=self.vocab_size).float()
+        y = F.one_hot(torch.tensor(self.pad_id(data[1:]), dtype=torch.long), num_classes=self.vocab_size).float()
+        return (x, y)
         
 
 class Encoder(nn.Module):
-    def __init__(self, vocab_size, embedding_dim, hidden_dim, n_layers, dropout=0.2):
+    def __init__(self, vocab_size, hidden_dim, n_layers, dropout=0.2):
         super().__init__()
-        self.embedding = nn.Embedding(vocab_size, embedding_dim)
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim, n_layers, dropout=dropout)
+        self.lstm = nn.LSTM(vocab_size, hidden_dim, n_layers, dropout=dropout)
     
     def forward(self, x):
-        embed = self.embedding(x)
-        logits, (h, c) = self.lstm(embed)
+        # x :  (batch_size, max_seq_len)
+        embed = x.transpose(0, 1) # (max_seq_len, batch_size)
+        logits, (h, c) = self.lstm(embed) # logits : (max_seq_len, batch_size, embed_dim), h, c: (n_layer, batch_size, embed_dim)
         return logits, h, c
 
 class Decoder(nn.Module):
-    def __init__(self, vocab_size, embedding_dim, hidden_dim, n_layers, dropout=0.2):
+    def __init__(self, vocab_size, hidden_dim, n_layers, dropout=0.2):
         super().__init__()
-        self.embedding = nn.Embedding(vocab_size, embedding_dim)
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim, n_layers, dropout=dropout)
+        self.lstm = nn.LSTM(vocab_size, hidden_dim, n_layers, dropout=dropout)
         self.classifier = nn.Linear(hidden_dim, vocab_size)
         
     def forward(self, x, h, c):
-        embed = self.embedding(x)
-        logits, (h, c) = self.lstm(embed, (h, c))
-        pred = self.classifier(logits) # check 필요
+        # print(h.shape, c.shape, x.shape)
+        logits, (h, c) = self.lstm(x.transpose(0, 1), (h, c))
+        pred = self.classifier(logits) # (1, batch_size, vocab_size)
         return pred, h, c
              
 
 class Seq2Seq(nn.Module):
-    def __init__(self, vocab_size, embedding_dim, hidden_dim, n_layers=2, dropout=0.2):
-        self.encoder = Encoder(vocab_size, embedding_dim, hidden_dim, n_layers, dropout)
-        self.decoder = Decoder(vocab_size, embedding_dim, hidden_dim, n_layers, dropout)
+    def __init__(self, vocab_size, hidden_dim, n_layers=2, dropout=0.2):
+        super().__init__()
+        self.vocab_size = vocab_size
+        self.encoder = Encoder(vocab_size, hidden_dim, n_layers, dropout)
+        self.decoder = Decoder(vocab_size, hidden_dim, n_layers, dropout)
         self.loss_func = nn.CrossEntropyLoss()
     
-    def forward(self, x, teacher_forcing_ratio=0.5):
+    def forward(self, data, teacher_forcing_ratio=1.0):
+        x, y = data
         
         _, h, c = self.encoder(x)
-        
         preds = torch.zeros_like(x)
-        
         sub_x = None
-        for i in range(0, len(x)):
-            sub_x = x[:, 0]
+        loss = 0
+        sub_x = x[:, 0:1, :]
+        for i in range(1, x.shape[1]):
             pred, h, c = self.decoder(sub_x, h, c)
-            
-            preds[:, i, :] = pred
+            pred = pred.transpose(0,1) 
+            preds[:, i, :] = pred[0]
             teacher_forcing = random.random() < teacher_forcing_ratio
             
-            sub_x = x[i] if teacher_forcing else preds.argmax(1)
+            pred = pred.argmax(2)
+            pred = F.one_hot(pred, self.vocab_size).float()
 
-        loss = self.loss_func(preds, x)
+            sub_x = x[:, i:i+1, :] if teacher_forcing else pred
+            loss += self.loss_func(preds[:, i, :].to('cuda'), y[:, i])
+        loss = loss / x.shape[1]
         return preds, loss
